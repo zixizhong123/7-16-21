925b860f59cf43ae0f08ac7db1ec835da40f08cf
zixizhong123 <zixi.zhong567@gmail.com>
2021-07-23 14:45:54 -0700

    solution for 3 coding problems
    dynamic array
    left rotation
    sparse_array

---
+++ left_rotation.py
@@ -0,0 +1,14 @@
+def rotateLeft(d, arr):
+    # Write your code here
+    result = []
+    num = len(arr)
+    ed_pt = d% num
+    result.extend(arr[ed_pt::])
+    result.extend(arr[0:ed_pt])
+    return result
+
+
+
+arr = [1,2,3,4,5]
+d = 4
+print(rotateLeft(d, arr))
